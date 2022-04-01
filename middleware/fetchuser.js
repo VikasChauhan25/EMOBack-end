c53138809649ea39861a888e1ee0c88d19f03359
Vikas Chauhan <vikaschauhan10001jnp@gmail.com>
2022-04-02 00:41:05 +0530

    jwt changes

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET="Vikasisagoodboy";
+const JWT_SECRET="Vikasisagoodaboy";
 const fetchuser=(req,res,next)=>{
     //Get the user from the jwt token and add id req object
     const token=req.header('auth-token');
@@ -8,6 +8,7 @@ const fetchuser=(req,res,next)=>{
         res.status(401).send({error:"Please authenticate using a valid token"})
     }
     try{
+        
         const data=jwt.verify(token,JWT_SECRET);
         req.user=data.user;
         next();
